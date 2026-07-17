a824de9cc61423e15d2cfcc068bf2e83fdd7a209
Zubin Dittia <zubin-dittia@users.noreply.github.com>
2026-07-17 02:57:46 -0700

    Deploy door app 2026-07-17

--- door/flutter_bootstrap.js
+++ door/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2694331427" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2077874900" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
